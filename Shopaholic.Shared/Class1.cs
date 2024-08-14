160a03189424f5d497cf287cfa4afa00ab7757e3
rhamb <rhamb@RACHELYOGA>
2024-08-13 17:29:32 -0700

    Add project files.

---
+++ Shopaholic.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Shopaholic.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Stopaholic.server/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Entity Framework and PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
