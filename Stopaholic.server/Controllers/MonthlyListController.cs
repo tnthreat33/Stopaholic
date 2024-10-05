33ded08ea167a57036748c7f4ba18e1e179fbc70
rhamb <rhamb@RACHELYOGA>
2024-10-05 12:02:50 -0400

    Add MonthlyList Controller

---
+++ Stopaholic.server/Controllers/MonthlyListController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Stopaholic.Shared.Models;
+using Stopaholic.Server.Services;
+
+namespace Stopaholic.Server.Controllers
+{
+    [ApiController]
+    [Route("/monthly-list")]
+    public class MonthlyListController : ControllerBase
+    {
+        private readonly IMonthlyListService _monthlyListService;
+
+        public MonthlyListController(IMonthlyListService monthlyListService)
+        {
+            _monthlyListService = monthlyListService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateMonthlyList(MonthlyList monthlyList)
+        {
+            if (monthlyList == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            await _monthlyListService.AddMonthlyListAsync(monthlyList);
+            return Ok(monthlyList);
+        }
+    }
+}
