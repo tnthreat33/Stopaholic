4a4bbec32f85b1e0ab614e481e1f25645b132de8
rhamb <rhamb@RACHELYOGA>
2024-10-05 11:59:36 -0400

    Add MonthlyList Service

---
+++ Stopaholic.server/Services/MonthlyListService.cs
@@ -0,0 +1,21 @@
+﻿using Stopaholic.Shared.Models;
+using Stopaholic.Server.Repositories;
+
+namespace Stopaholic.Server.Services
+{
+    public class MonthlyListService : IMonthlyListService
+    {
+        private readonly IMonthlyListRepository _monthlyListRepository;
+
+        public MonthlyListService(IMonthlyListRepository monthlyListRepository)
+        {
+            _monthlyListRepository = monthlyListRepository;
+        }
+
+        public async Task AddMonthlyListAsync(MonthlyList monthlyList)
+        {
+            
+            await _monthlyListRepository.AddMonthlyListAsync(monthlyList);
+        }
+    }
+}
